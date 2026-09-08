06a60af4fbb732c28bab9172aa00d35a37a4c87b
Heena-Sabharwal <heenasabharwal05@gmail.com>
2026-09-08 23:23:29 +0530

    Time: 65 ms (8.14%), Space: 78.4 MB (12.76%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+
+        for(int i=1;i<=nums.size()+1;i++){
+            if(!mp.count(i))
+                return i;
+        }
+        return 0;
+    }
+};
