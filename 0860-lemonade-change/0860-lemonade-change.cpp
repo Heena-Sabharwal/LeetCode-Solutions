d1a65fdb4a7884b290c5fd01d3a0cecbc18c632e
Heena-Sabharwal <heenasabharwal05@gmail.com>
2026-09-23 12:42:32 +0530

    Time: 2 ms (11.8%), Space: 87.3 MB (10.9%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int,int>mp;
+        mp[5]=0;mp[10]=0;
+
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                mp[5]++;
+            }
+            else if(bills[i]==10){
+                if(mp[5]){
+                    mp[5]--;
+                }
+                else{
+                    return false;
+                }
+                mp[10]++;
+            }
+            else{
+                if(mp[5] && mp[10]){
+                    mp[5]--;
+                    mp[10]--;
+                }
+                else if(mp[5]>=3)
+                    mp[5]-=3;
+                else
+                    return false;
+            }
+
+        }
+
+        return true;
+    }
+};
